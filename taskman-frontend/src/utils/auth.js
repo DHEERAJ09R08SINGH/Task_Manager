62ad694e959ae3a8fe1b6afc8c40222428df03bc
DHEERAJ R SINGH <newdheerajsingh08@gmail.com>
2026-05-02 23:36:38 +0530

    delete frontend

--- taskman-frontend/src/utils/auth.js
+++
@@ -1,8 +0,0 @@
-export const getUser = () => {
-  return JSON.parse(localStorage.getItem("user") || "null");
-};
-
-export const getRole = () => {
-  const user = getUser();
-  return user?.role;
-};
